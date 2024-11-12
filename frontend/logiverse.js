852f46fa8da8c920f328612ea7a0223f8dbaa334
block36_ <block36not33@gmail.com>
2024-11-11 22:51:31 -0500

    very basic implementation with hardcoded response

---
+++ frontend/logiverse.js
@@ -0,0 +1,59 @@
+export default {};
+
+// Login instances copied from svenlaa's loggo instance 
+// https://github.com/Svenlaa/svenlaa.com/blob/main/playground/loggo/logiverse.js
+// There doesn't seem to be any licensing info, but that's probably fine
+
+export const config = {
+    version: '0.0.1',
+    instance_name: 'login.block36.gay',
+    instance_admin: 'block36',
+    endpoints: {
+      update: 'http://127.0.0.1:4242/update',
+        login: 'http://127.0.0.1:4242/login',
+        delete: 'http://127.0.0.1:4242/delete',
+        ban: 'http://127.0.0.1:4242/ban',
+    },
+    instances: [
+        {
+            name: 'todepond.com',
+            feed: 'https://todepond-lablogingetusers.web.val.run',
+            gifs: {
+                berd: {
+                    src: 'https://www.todepond.com/image/berd.gif',
+                    alt: 'A sparkly bird gif',
+                },
+                bot: {
+                    src: 'https://www.todepond.com/image/bot.gif',
+                    alt: 'A sparkly robot gif',
+                },
+                tode: {
+                    src: 'https://www.todepond.com/image/tode.gif',
+                    alt: 'A sparkly toad gif',
+                },
+            },
+        },
+        {
+            name: 'svenlaa.com',
+            feed: 'https://svenlaa-lablogingetusers.web.val.run',
+            // update: "https://svenlaa-labloginupdatestatus.web.val.run",
+            // login: "https://svenlaa-lablogin.web.val.run",
+            // delete: "https://svenlaa-lablogindeleteaccount.web.val.run",
+            // ban: "https://svenlaa-labloginbanuser.web.val.run",
+        },
+        {
+            name: 'evolved.systems',
+            feed: 'https://evol-lablogingetusers.web.val.run',
+            // update: "https://evol-labloginupdatestatus.web.val.run",
+        },
+        {
+            name: 'rossilaz.xyz',
+            feed: 'https://mittzy-loginredux_getusers.web.val.run',
+        },
+        {
+            _comment: 'client on github pages @ https://cute-catgirl.github.io/login/',
+            name: 'maemoon.me',
+            feed: 'https://maemoon-lablogingetusers.web.val.run',
+        },
+    ],
+};
---
+++ server.lisp
@@ -0,0 +1,54 @@
+;; The server sided stuff for my login instance
+
+(load "~/quicklisp/setup.lisp")
+
+(ql:quickload '(clack alexandria))
+
+(defparameter *clack-server*
+  (clack:clackup (lambda (env) (funcall 'handler env))
+    :port 4242))
+
+(defun handler (env)
+  (destructuring-bind (&key request-method path-info request-uri
+                            query-string headers &allow-other-keys) env
+    (princ (format nil "~S request for ~S: ~S" request-method request-uri query-string))
+    (terpri)
+    (cond
+      ((alexandria:starts-with-subseq "/index.html" path-info)
+        `(200 
+          nil 
+          (,(file-get-contents "frontend/index.html"))))
+
+      ((alexandria:starts-with-subseq "/lab/login" path-info)
+        `(200 
+          nil 
+          (,(file-get-contents "frontend/index.html"))))
+
+      ((alexandria:starts-with-subseq "/logiverse.js" path-info)
+        `(200 
+          nil 
+          (,(file-get-contents "frontend/logiverse.js"))))
+
+      ((alexandria:starts-with-subseq "/update" path-info) 
+       (princ "Update")
+       (terpri))
+
+      ((alexandria:starts-with-subseq "/getUsers" path-info) 
+       `(200
+         null
+         ("[[\"This is a successful test\", \"me! :3\", \"2024-11-11 23:58:45\", null, 0]]")))
+
+      (T
+        `(404
+          nil
+          (,(format nil "Not found <br><br> Method: ~S Path: ~S URI: ~A Query: ~S~%Headers: ~S"
+                    request-method path-info request-uri query-string
+                    (alexandria:hash-table-alist headers))))))))
+
+(defun file-get-contents (filename)
+  (with-open-file (stream filename)
+    (let ((contents (make-string (file-length stream))))
+      (read-sequence contents stream)
+      contents)))
+
+(sleep most-positive-fixnum)
