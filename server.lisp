77c9a71977e2f9fccda33cb393460d6f157598ee
block36_ <block36not33@gmail.com>
2024-11-16 21:49:05 -0500

    some functions for interacting with sqlite

--- server.lisp
+++ server.lisp
@@ -2,7 +2,51 @@
 
 (load "~/quicklisp/setup.lisp")
 
-(ql:quickload '(clack alexandria com.inuoe.jzon))
+(ql:quickload '(clack alexandria com.inuoe.jzon mito))
+
+(defconstant table-name "users")
+
+(mito:connect-toplevel :sqlite3
+                       :database-name "user-data")
+
+(mito:deftable user ()
+  ((username :col-type (:text) 
+             :primary-key T)
+   (password :col-type (:text))
+   (status   :col-type (:text))
+   (gif      :col-type (:text))))
+
+(defun new-user (username password &optional provided-status provided-gif)
+  (let ((status (if provided-status
+                    provided-status
+                    "Hello I am new"))
+        (gif    (if provided-gif
+                    provided-gif
+                    "none")))
+    (make-instance 'user :username username 
+                         :password password 
+                         :status   status
+                         :gif      gif)))
+
+(defun get-user (username &optional password)
+  (if (not password) 
+    (mito:find-dao 'user :username username)
+    (mito:find-dao 'user :username username
+                         :password password)))
+
+(mito:ensure-table-exists 'user)
+
+(defun update-user (user-data)
+  (if (get-user (slot-value user-data 'username))
+    (progn 
+      (mito:delete-dao user-data)
+      (mito:save-dao user-data))
+    (mito:insert-dao user-data)))
+
+(unless (get-user "test")
+  (mito:insert-dao (new-user "test" "12345")))
+
+(update-user (new-user "test" "securepasswordwow"))
 
 (defparameter *clack-server*
   (clack:clackup (lambda (env) (funcall 'handler env))
@@ -64,7 +108,9 @@
       (return-from handle-login (error-response "Missing username or password")))
 
     (if (contains-banned-chars username) (return-from handle-login (error-response "Invalid username")))
+    (if (> (length username) 50) (return-from handle-login (error-response "Username is longer than 50 characters")))
     (if (contains-banned-chars password) (return-from handle-login (error-response "Invalid password")))
+    (if (> (length password) 50) (return-from handle-login (error-response "Password is longer than 50 characters")))
     (princ (format nil "username: ~A password: ~A" username password))
     (terpri)))
 
